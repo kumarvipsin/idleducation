65e8a562b76ebeb8d5c915af55a8e646dd5e20c4
Amod Sharma <kumarvips.in@gmail.com>
2025-08-23 17:13:21 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section class="w-

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
 
       <section className="w-full py-12 md:py-24 lg:py-32 bg-primary text-primary-foreground">
         <div className="container px-4 md:px-6">
-          <div className="flex flex-wrap items-center justify-center gap-8 lg:gap-16">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 justify-items-center">
             {stats.map((stat, index) => (
               <div key={index} className="flex flex-col items-center text-center">
                 <div 
