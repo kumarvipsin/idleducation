2b672441fe1d6d4f8a0687d67a67aaaf14b2122e
Amod Sharma <kumarvips.in@gmail.com>
2025-08-24 19:16:47 +0000

    change name toggle theme to Dark/Light

--- src/components/settings-toggle.tsx
+++ src/components/settings-toggle.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -34,7 +35,7 @@ export function SettingsToggle() {
           <DropdownMenuSubTrigger>
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="ml-2">Toggle theme</span>
+            <span className="ml-2">Dark/Light</span>
           </DropdownMenuSubTrigger>
           <DropdownMenuPortal>
             <DropdownMenuSubContent>
