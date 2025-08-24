a7623356b27c72eb98885b6ae5134fd6e31b83a1
Amod Sharma <kumarvips.in@gmail.com>
2025-08-24 17:11:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Link from "next/link";
 import { BookOpen, LogIn, Menu, Phone, Mail, Home as HomeIcon, User as UserIcon, Facebook, Twitter, Instagram, Info, Globe } from "lucide-react";
 import { Button } from "./ui/button";
