95dd6f9b26b1e9655ab1ef102decc3e2e4427a71
Amod Sharma <kumarvips.in@gmail.com>
2025-08-26 08:38:01 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ToppersTestimonia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <HeroSection />
       <PopularPrograms />
       <OurFeatures />
-      <ExpertTeam />
       <ToppersTestimonials />
+      <ExpertTeam />
       <StudentTestimonials />
     </div>
   );
