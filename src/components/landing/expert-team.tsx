31eaaf4b0251e58b145ee026aa0aba0618d0e80a
Amod Sharma <kumarvips.in@gmail.com>
2025-08-26 16:38:09 +0000

    add this photo to public dir and map for Biology & Chemistry

--- src/components/landing/expert-team.tsx
+++ src/components/landing/expert-team.tsx
@@ -14,7 +14,7 @@ export function ExpertTeam() {
         name: t('team.member1.name'),
         designation: t('team.member1.designation'),
         experience: t('team.member1.experience'),
-        avatar: "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxmZW1hbGUlMjB0ZWFjaGVyfGVufDB8fHx8MTc1NjIwMDUwNXww&ixlib=rb-4.1.0&q=80&w=1080",
+        avatar: "/amod-sharma.jpg",
         avatarHint: "male teacher"
     },
     {
