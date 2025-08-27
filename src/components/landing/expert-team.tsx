39c70738478e47885c52de1316b7d6d5ed131048
Amod Sharma <kumarvips.in@gmail.com>
2025-08-27 11:42:58 +0000

    find error and fix it

--- src/components/landing/expert-team.tsx
+++ src/components/landing/expert-team.tsx
@@ -14,7 +14,7 @@ export function ExpertTeam() {
         name: t('team.member1.name'),
         designation: t('team.member1.designation'),
         experience: t('team.member1.experience'),
-        avatar: "/amod.jpeg",
+        avatar: "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxtYWxlJTIwdGVhY2hlcnxlbnwwfHx8fDE3NTYyMDAyODl8MA&ixlib=rb-4.1.0&q=80&w=1080",
         avatarHint: "male teacher"
     },
     {
