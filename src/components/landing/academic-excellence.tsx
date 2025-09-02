4c6b2f9d31a89360fabde73bcba9b196f97e7912
Amod Sharma <kumarvips.in@gmail.com>
2025-09-02 05:56:03 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/landing/academic-excellence.tsx
+++ src/components/landing/academic-excellence.tsx
@@ -17,7 +17,7 @@ const resultsData = {
     hint: 'classroom students'
   },
   'CBSE 12': {
-    image: 'https://images.unsplash.com/photo-1541339907198-e08756dedf3f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
+    image: 'https://images.unsplash.com/photo-1541339907198-e08756dedf3f?q=80&w=2070&auto-format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
     alt: 'CBSE 12th Toppers',
     hint: 'graduation ceremony'
   },
@@ -27,7 +27,7 @@ const resultsData = {
     hint: 'students studying'
   },
   'NEET': {
-    image: 'https://images.unsplash.com/photo-1576091160550-2173dba999ef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
+    image: 'https://images.unsplash.com/photo-1576091160550-2173dba999ef?q=80&w=2070&auto-format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
     alt: 'NEET Toppers',
     hint: 'medical students'
   },
