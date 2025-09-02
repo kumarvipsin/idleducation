a1bf362e37bb86b429724ca38ba489d110f3a6c8
Amod Sharma <kumarvips.in@gmail.com>
2025-09-02 15:57:52 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ChatBot>
    
    with th

--- src/components/chat-bot.tsx
+++ src/components/chat-bot.tsx
@@ -28,10 +28,10 @@ export function ChatBot() {
         <Button
           asChild
           size="icon"
-          className="relative rounded-full w-12 h-12 shadow-lg bg-gradient-to-br from-gray-800 to-black hover:from-gray-700 hover:to-gray-900 text-white transition-transform hover:scale-110"
+          className="relative rounded-full w-12 h-12 shadow-lg bg-gradient-to-br from-red-600 to-black hover:from-red-700 hover:to-black text-white transition-transform hover:scale-110"
         >
           <Link href="tel:+918860040010">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-600 opacity-75"></span>
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
             <Phone className="h-6 w-6 relative" />
             <span className="sr-only">Call Us</span>
           </Link>
