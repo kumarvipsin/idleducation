7f09ef72896b601677e61cc2494611adc0b28be3
Amod Sharma <kumarvips.in@gmail.com>
2025-09-14 07:37:03 +0000

    i unable to seel all classes  in phone, fix this  (_for element div_)

--- src/app/resources/reference-books/page.tsx
+++ src/app/resources/reference-books/page.tsx
@@ -140,7 +140,7 @@ export default function ReferenceBooksPage() {
 
       <div className="bg-muted/50 rounded-lg p-4 mb-8">
         <div className="overflow-x-auto pb-2 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
-            <div className="flex justify-center items-center gap-2 whitespace-nowrap px-4 sm:px-0">
+            <div className="flex justify-start md:justify-center items-center gap-2 whitespace-nowrap px-4 sm:px-0">
               {allCategories.map((c) => (
                 <button
                   key={c}
