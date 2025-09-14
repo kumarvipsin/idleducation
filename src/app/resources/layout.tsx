4caebcb0f30fb630374a3703b79d6dd975c696f7
Amod Sharma <kumarvips.in@gmail.com>
2025-09-14 12:12:10 +0000

    mak 50% small contant to look better (_for element <CardContent>_)

--- src/app/resources/layout.tsx
+++ src/app/resources/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
 
 export default function ResourcesLayout({ children }: { children: ReactNode }) {
-  return <div className="py-12 px-4 md:px-6 lg:container lg:mx-auto">{children}</div>;
+  return <div className="py-12 px-4 md:px-6">{children}</div>;
 }
