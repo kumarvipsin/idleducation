69282b607e964ec6bc399d1048ea381598f18034
Amod Sharma <kumarvips.in@gmail.com>
2025-09-15 07:20:31 +0000

    give margen selected section both left and right side 10% only desktop m

--- src/app/resources/layout.tsx
+++ src/app/resources/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
 
 export default function ResourcesLayout({ children }: { children: ReactNode }) {
-  return <div className="py-12 px-4 md:px-6">{children}</div>;
+  return <div className="py-12 px-4 md:px-[10%]">{children}</div>;
 }
