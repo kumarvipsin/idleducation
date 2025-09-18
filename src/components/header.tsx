dd1bb0d52ed6f64d18b72a59b91fd265dd67f1d3
Amod Sharma <kumarvips.in@gmail.com>
2025-09-18 19:06:14 +0000

    apply this "dee2e6" in bg (_for element div_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -414,7 +414,7 @@ export function Header() {
 
   return (
     <Collapsible asChild open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
-      <header className="sticky top-0 z-50 bg-[#f8f9fa] border-b">
+      <header className="sticky top-0 z-50 bg-[#dee2e6] border-b">
           <div className="bg-[#ED1C24] text-white text-xs">
               <div className="container mx-auto px-4 md:px-[10%] flex justify-end items-center h-8">
                   <div className={cn("items-center flex gap-x-2 md:gap-x-4")}>
