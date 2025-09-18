fce1e826d4b859776faa4dcce81c5ce125fa260b
Amod Sharma <kumarvips.in@gmail.com>
2025-09-18 05:40:15 +0000

    incrage this "Get in Touch" text siz  (_for element h2_)

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -372,7 +372,7 @@ export default function ContactPage() {
             <Card className="shadow-lg overflow-hidden flex flex-col md:flex-row border-t-8 border-primary rounded-t-lg">
               <div className="p-8 space-y-8 md:w-2/5 bg-white dark:bg-card">
                 <div className="space-y-2">
-                  <h2 className="text-2xl font-bold text-primary">Get in Touch</h2>
+                  <h2 className="text-3xl font-bold text-primary">Get in Touch</h2>
                   <p className="text-muted-foreground">We're here to help and answer any question you might have. We look forward to hearing from you.</p>
                 </div>
                 
@@ -412,7 +412,7 @@ export default function ContactPage() {
                 </div>
               </div>
 
-              <div className="p-8 md:w-3/5 border rounded-lg m-2">
+              <div className="p-8 md:w-3/5">
                 <Form {...contactForm}>
                   <form onSubmit={contactForm.handleSubmit(onContactSubmit)} className="space-y-4">
                     <FormField
