24cc66a9ddff1514d6375e1b623284c2fa306a42
Amod Sharma <kumarvips.in@gmail.com>
2025-09-20 12:29:33 +0000

    updated

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,12 @@
 import { ReactNode } from 'react';
-
+import { Header } from '@/components/header';
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
+    <>
+    <Header></Header>
     <main className="flex flex-col flex-grow items-center justify-center bg-muted/40">
       {children}
     </main>
+    </>
   );
 }
