4f99860cd5c8d679af5eea22db2836f63fd8d88c
Amod Sharma <kumarvips.in@gmail.com>
2025-09-20 14:36:27 +0000

    on `next/image`, hostname "storage.googleapis.com" is not configured und

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
