2443d46c522ef23e4da17cd6d3545130f3ba3548
Amod Sharma <kumarvips.in@gmail.com>
2025-09-27 19:59:03 +0000

    its good, but make some more modern look

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -408,7 +408,8 @@ const SidebarContent = React.forwardRef<
       ref={ref}
       data-sidebar="content"
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+        "flex min-h-0 flex-1 flex-col gap-1 overflow-auto p-1",
+        "group-data-[collapsible=icon]:overflow-hidden",
         className
       )}
       {...props}
