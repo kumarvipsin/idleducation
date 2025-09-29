2269c09fcd01055dadce5979cdbe66b3252dee5f
Amod Sharma <kumarvips.in@gmail.com>
2025-09-29 15:18:07 +0000

    incrage direction symbol 80 thik (_for element <Button>_)

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -216,7 +216,7 @@ const CarouselPrevious = React.forwardRef<
       onClick={scrollPrev}
       {...props}
     >
-      <ChevronLeft className="h-6 w-6" />
+      <ChevronLeft className="h-8 w-8" />
       <span className="sr-only">Previous slide</span>
     </Button>
   )
@@ -245,7 +245,7 @@ const CarouselNext = React.forwardRef<
       onClick={scrollNext}
       {...props}
     >
-      <ChevronRight className="h-6 w-6" />
+      <ChevronRight className="h-8 w-8" />
       <span className="sr-only">Next slide</span>
     </Button>
   )
