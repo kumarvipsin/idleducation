a57cc555492a5c1e9b34ba1fd89773ed9c0d2443
Amod Sharma <kumarvips.in@gmail.com>
2025-09-29 19:31:39 +0000

    remove this buton (_for element <Button>_)

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -229,26 +229,7 @@ const CarouselNext = React.forwardRef<
 >(({ className, variant = "outline", size = "icon", ...props }, ref) => {
   const { orientation, scrollNext, canScrollNext } = useCarousel()
 
-  return (
-    <Button
-      ref={ref}
-      variant={variant}
-      size={size}
-      className={cn(
-        "absolute h-12 w-12 rounded-full bg-gray-200/50 hover:bg-gray-200/80 shadow-md opacity-0 group-hover:opacity-100 transition-opacity",
-        orientation === "horizontal"
-          ? "-right-16 top-1/2 -translate-y-1/2"
-          : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
-        className
-      )}
-      disabled={!canScrollNext}
-      onClick={scrollNext}
-      {...props}
-    >
-      <ChevronRight className="h-8 w-8" />
-      <span className="sr-only">Next slide</span>
-    </Button>
-  )
+  return null;
 })
 CarouselNext.displayName = "CarouselNext"
 
