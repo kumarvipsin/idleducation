de8d87a33c9055764e589a3e6cf26bc2844ccbef
Amod Sharma <kumarvips.in@gmail.com>
2025-09-30 18:36:58 +0000

    this selected line bilow of nevbar apply sutable gradiunt clour mact wit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -400,7 +400,7 @@ export function Header() {
                   </CollapsibleTrigger>
                 </div>
           </div>
-           <div className={cn("h-[2px] bg-gradient-to-r from-red-500 via-purple-500 to-blue-500 animate-gradient-x transition-transform duration-300", show ? 'scale-x-100' : 'scale-x-0')} style={{transformOrigin: 'left'}}></div>
+           <div className={cn("h-[2px] bg-gradient-to-r from-blue-400 via-purple-400 to-blue-400 animate-gradient-x transition-transform duration-300", show ? 'scale-x-100' : 'scale-x-0')} style={{transformOrigin: 'left'}}></div>
           <CollapsibleContent asChild>
             <div className={cn(
               "overflow-hidden transition-all data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down md:hidden", "duration-200"
