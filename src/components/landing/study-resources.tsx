b5c2e9b0f46f9b156782afd6ec2e8690fbe16937
Amod Sharma <kumarvips.in@gmail.com>
2025-09-30 08:35:16 +0000

    replace this text in to "Previous Year Paper" (_for element h3_)

--- src/components/landing/study-resources.tsx
+++ src/components/landing/study-resources.tsx
@@ -25,7 +25,7 @@ const resources = [
   },
   {
     category: "EXAM PREP",
-    title: "Previous Year Questions",
+    title: "Previous Year Paper",
     description: "Sharpen your skills and get exam-ready by practicing with past papers.",
     href: "/resources/previous-year-questions",
     imageUrl: "https://images.unsplash.com/photo-1724080924541-a9bbb5953229?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxMXx8ZXhhbXxlbnwwfHx8fDE3NTkxNzQ2OTB8MA&ixlib=rb-4.1.0&q=80&w=1080",
