b75df029055b716906c4fae87b0d2e1237815e8f
Amod Sharma <kumarvips.in@gmail.com>
2025-10-02 20:47:55 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -129,14 +129,12 @@ export default function StorePage() {
     };
     
     const renderSkeleton = () => (
-      <div className="overflow-x-auto pb-8 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
-        <div className="flex gap-6 px-4 md:px-[10%]">
-          {[...Array(4)].map((_, index) => (
-              <div key={index} className="block flex-shrink-0 w-[300px] sm:w-[350px]">
-                  <Skeleton className="h-[450px] w-full rounded-lg" />
-              </div>
-          ))}
-        </div>
+      <div className="flex gap-6 px-4 md:px-[10%]">
+        {[...Array(4)].map((_, index) => (
+            <div key={index} className="block flex-shrink-0 w-[300px] sm:w-[350px]">
+                <Skeleton className="h-[450px] w-full rounded-lg" />
+            </div>
+        ))}
       </div>
     );
 
@@ -190,11 +188,11 @@ export default function StorePage() {
                     </div>
                      
                     <div className="relative">
-                         {loading ? (
-                            renderSkeleton()
-                         ) : (
-                            <div className="overflow-x-auto pb-8 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
-                                 <div className="flex gap-6 px-4 md:px-[10%]">
+                        <div className="overflow-x-auto pb-8 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+                             {loading ? (
+                                renderSkeleton()
+                             ) : (
+                                <div className="flex gap-6 px-4 md:px-[10%]">
                                     {filteredBooks.map((book, index) => (
                                         <div key={book.id} className="block flex-shrink-0 w-[300px] sm:w-[350px] group">
                                         <Card className="overflow-hidden shadow-md hover:shadow-xl transition-all duration-300 animate-fade-in-up group rounded-lg bg-card flex flex-col h-full" style={{ animationDelay: `${index * 50}ms` }}>
@@ -240,11 +238,10 @@ export default function StorePage() {
                                             </CardContent>
                                         </Card>
                                         </div>
-                                    ))
-                                 )}
+                                    ))}
                                 </div>
-                            </div>
-                        )}
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
