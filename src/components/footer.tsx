dd56697242a8025adc0d5f86011fe960fd737c69
Amod Sharma <kumarvips.in@gmail.com>
2025-10-02 18:07:20 +0000

    make this image fit to max this of this section (_for element img_)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -115,7 +115,7 @@ export function Footer() {
             </div>
         </div>
         <div className="bg-gray-200 dark:bg-gray-900 border-t border-gray-300 dark:border-gray-700">
-            <div className="container mx-auto px-4 md:px-6 py-2">
+            <div className="container mx-auto px-4 md:px-6 py-1">
                 <div className="flex flex-col sm:flex-row justify-between items-center text-[0.6rem]">
                     <div className="flex items-center gap-2">
                         <Image 
