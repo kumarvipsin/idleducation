9fab82f8e60da21bf20eb2088cb2af565ab8a40f
Amod Sharma <kumarvips.in@gmail.com>
2025-10-07 08:31:11 +0000

    remove bg make only outline, dont use all capital fount only make fit le

--- src/components/foundation/donation-categories.tsx
+++ src/components/foundation/donation-categories.tsx
@@ -175,7 +175,7 @@ export function DonationCategories({ donationCategories, openDonateDialog, isDon
                 </div>
                 <div className="relative">
                     <div className="overflow-x-auto pb-8 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
-                        <div className="flex gap-6 px-4 md:px-[10%]">
+                        <div className="flex gap-6 pl-4 md:pl-[10%]">
                             {donationCategories.map((category, index) => {
                                 const percentage = category.goal > 0 ? (category.raised / category.goal) * 100 : 0;
                                 return (
@@ -210,8 +210,10 @@ export function DonationCategories({ donationCategories, openDonateDialog, isDon
                                                 </div>
 
                                                 <div className="mt-6 text-left">
-                                                    <Button variant="outline" size="sm" className="font-bold rounded-full shadow-md hover:shadow-lg transition-all duration-300 ease-in-out bg-white text-primary hover:bg-gray-100">
-                                                        READ MORE
+                                                    <Button asChild variant="outline" size="sm" className="font-bold rounded-full shadow-md hover:shadow-lg transition-all duration-300 ease-in-out text-primary">
+                                                        <Link href="#">
+                                                            Read More
+                                                        </Link>
                                                     </Button>
                                                 </div>
                                             </CardContent>
