a957779b4956a3c7310945e01def9b681303ac99
Amod Sharma <kumarvips.in@gmail.com>
2025-12-29 16:49:52 +0000

    restyle this section (_for element <OurFeatures>_)

--- src/components/landing/our-features.tsx
+++ src/components/landing/our-features.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { Card, CardContent } from "@/components/ui/card";
@@ -44,7 +43,7 @@ export function OurFeatures() {
   return (
     <section 
       className="w-full relative py-12 md:py-16 bg-cover bg-center bg-no-repeat"
-      style={{backgroundImage: "url('https://images.unsplash.com/photo-1543269865-cbf427effbad?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8')"}}
+      style={{backgroundImage: "url('https://images.unsplash.com/photo-1543269865-cbf427effbad?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"}}
     >
       <div className="absolute inset-0 bg-black/50 backdrop-blur-sm"></div>
       <div className="container mx-auto px-4 md:px-[10%] relative z-10">
