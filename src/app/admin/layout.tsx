0f4c0b8ab5457c622f2c316c35d5a315f3bce89e
Amod Sharma <kumarvips.in@gmail.com>
2026-01-01 15:09:51 +0000

    remove this  (_for element div_)

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -311,15 +311,6 @@ function AdminLayout({
                         </Button>
                       </DropdownMenuTrigger>
                       <DropdownMenuContent className="w-56" align="end" forceMount>
-                        <DropdownMenuLabel className="font-normal">
-                          <div className="flex flex-col space-y-1">
-                            <p className="text-sm font-medium leading-none">{user.name}</p>
-                            <p className="text-xs leading-none text-muted-foreground">
-                              {user.email}
-                            </p>
-                          </div>
-                        </DropdownMenuLabel>
-                        <DropdownMenuSeparator />
                         <DropdownMenuItem asChild>
                             <SidebarTrigger className="w-full justify-start">
                                 <Menu className="mr-2 h-4 w-4" />
