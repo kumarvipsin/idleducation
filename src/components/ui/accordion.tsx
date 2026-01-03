d6cd2a960f3fe66582b16a2863eaf1d2ecee9f23
Amod Sharma <kumarvips.in@gmail.com>
2026-01-03 04:11:08 +0000

    dont change icon clour use similar clour given image

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -29,15 +29,13 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>div>svg]:rotate-180",
+        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
         className
       )}
       {...props}
     >
       {children}
-      <div className="flex h-6 w-6 shrink-0 items-center justify-center rounded-full bg-primary text-primary-foreground">
-        <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
-      </div>
+      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
