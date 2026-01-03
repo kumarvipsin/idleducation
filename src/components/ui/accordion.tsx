da5d53f5c2e691717a6ac4e24e1a6489e9b877ed
Amod Sharma <kumarvips.in@gmail.com>
2026-01-03 04:12:02 +0000

    use given image like icon and clour same

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -29,13 +29,15 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>div>svg]:rotate-180",
         className
       )}
       {...props}
     >
       {children}
-      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      <div className="flex h-6 w-6 items-center justify-center rounded-full bg-blue-600 text-white">
+        <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      </div>
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
