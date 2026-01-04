581d74644ff3076f0f3fd5d9f6c6079ddbeb6064
Amod Sharma <kumarvips.in@gmail.com>
2026-01-04 10:50:51 +0000

    replace into "For Competitive Exams" (_for element h3_)

--- src/components/landing/exam-categories.tsx
+++ src/components/landing/exam-categories.tsx
@@ -22,7 +22,7 @@ const categories = [
     imageHint: "student studying"
   },
   {
-    title: "Competitive Exams",
+    title: "For Competitive Exams",
     subtitle: "Your gateway to success in government and national-level entrance exams.",
     href: "/examcat",
     imageUrl: "https://images.unsplash.com/photo-1576267423445-b2e0074d68a4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
