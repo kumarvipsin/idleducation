72e39042734781b5f0c86728f5fc000a6a7708e3
Amod Sharma <kumarvips.in@gmail.com>
2026-01-04 10:43:44 +0000

    add sutable discraption small and power full (_for element h3_)

--- src/components/landing/exam-categories.tsx
+++ src/components/landing/exam-categories.tsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 const categories = [
   {
     title: "Academic Programs",
-    subtitle: "Class 6 - 12",
+    subtitle: "Your complete guide to school-level success.",
     href: "/school",
     imageUrl: "https://images.unsplash.com/photo-1577896851231-70ef18881754?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     imageHint: "students in classroom"
