224b24bfb7ad11cf2bfec2a36d074caaa6e7325e
Amod Sharma <kumarvips.in@gmail.com>
2026-01-04 10:44:39 +0000

    add sutable discraption small and power full (_for element h3_)

--- src/components/landing/exam-categories.tsx
+++ src/components/landing/exam-categories.tsx
@@ -23,7 +23,7 @@ const categories = [
   },
   {
     title: "Competitive Exams",
-    subtitle: "CUET | SSC | DELHI POLICE & OTHER EXAM",
+    subtitle: "Your gateway to success in government and national-level entrance exams.",
     href: "/examcat",
     imageUrl: "https://images.unsplash.com/photo-1576267423445-b2e0074d68a4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     imageHint: "professionals meeting"
