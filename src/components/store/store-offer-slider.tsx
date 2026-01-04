bd7bf133cad0d5c1233c1f2d5f23d0aee27206f7
Amod Sharma <kumarvips.in@gmail.com>
2026-01-04 10:26:43 +0000

    reduse 50% small hightits too large (_for element img_)

--- src/components/store/store-offer-slider.tsx
+++ src/components/store/store-offer-slider.tsx
@@ -40,7 +40,7 @@ export function StoreOfferSlider() {
                 <CarouselContent>
                     {offerSlides.map((slide, index) => (
                         <CarouselItem key={index}>
-                            <div className="relative w-full aspect-[16/6] rounded-lg overflow-hidden">
+                            <div className="relative w-full aspect-[16/4] rounded-lg overflow-hidden">
                                 <Image
                                     src={slide.src}
                                     alt={slide.alt}
