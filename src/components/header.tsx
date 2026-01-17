b17279276a7f9560e35a199c779f161be89e9340
Amod Sharma <kumarvips.in@gmail.com>
2026-01-17 15:15:08 +0000

    incrage log siz maximum siz to fix nevbar (_for element img_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -382,7 +382,7 @@ export function Header() {
         <header className={cn(headerClasses, 'z-50')}>
             <div className="container mx-auto px-4 md:px-6 flex justify-between items-center h-full">
                 <Link href={logoHref} className="flex items-center justify-center -ml-2">
-                  <Image src="/logo.png" alt="IDL Education Logo" width={40} height={40} className="h-10 w-auto" />
+                  <Image src="/logo.png" alt="IDL Education Logo" width={48} height={48} className="h-12 w-auto" />
                 </Link>
                 
                  <div className="flex-1 justify-start items-center gap-1 ml-4 hidden md:flex">
