1fe73ff396859f590b39bdec9fd14b066b38f93d
Amod Sharma <kumarvips.in@gmail.com>
2026-01-27 14:57:59 +0000

    remove this reference book card completly no need it  (_for element div_

--- src/components/landing/study-resources.tsx
+++ src/components/landing/study-resources.tsx
@@ -39,14 +39,6 @@ const resources = [
     imageUrl: "https://ezeenotes.in/wp-content/uploads/2024/03/Book-Mockups-2-1-e1710253086447-1024x802.png",
     imageHint: "exam paper test",
   },
-  {
-    category: "FURTHER READING",
-    title: "Reference Books",
-    description: "Explore a curated collection of reference books to supplement your learning.",
-    href: "/resources/reference-books",
-    imageUrl: "https://ezeenotes.in/wp-content/uploads/2024/03/Book-Mockups-2-1-e1710253086447-1024x802.png",
-    imageHint: "library books",
-  },
 ];
 
 export function StudyResources() {
