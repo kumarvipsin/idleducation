bc17371fb133e1d677d92612d33d99c31caa700d
Amod Sharma <kumarvips.in@gmail.com>
2026-01-27 14:23:40 +0000

    make this page with full siz  (_for element div_)

--- src/app/resources/ncert-solutions/[...slug]/page.tsx
+++ src/app/resources/ncert-solutions/[...slug]/page.tsx
@@ -112,7 +112,7 @@ function NcertSolutionsDetailsContent() {
 
 export default function NcertSolutionsDetailsPage() {
     return (
-        <div className="container mx-auto px-4 md:px-[10%] py-8">
+        <div className="w-full">
             <Suspense fallback={<Skeleton className="h-screen w-full" />}>
                 <NcertSolutionsDetailsContent />
             </Suspense>
