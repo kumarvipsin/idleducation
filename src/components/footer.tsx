5e74efb109b41194ca12aef69d4e755e914322e9
Amod Sharma <kumarvips.in@gmail.com>
2026-02-06 07:01:46 +0000

    make consistance all gray like visual colour

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -53,10 +53,10 @@ export function Footer() {
                     </p>
                     <div className="flex items-center justify-start gap-4">
                         {[
-                          { icon: <Instagram className="h-5 w-5 text-rose-400" />, label: "Instagram", href: "https://www.instagram.com/idleducation" },
-                          { icon: <Facebook className="h-5 w-5 text-blue-500" />, label: "Facebook", href: "https://www.facebook.com/idleducation" },
-                          { icon: <Twitter className="h-5 w-5 text-sky-400" />, label: "Twitter", href: "https://x.com/idleducation" },
-                          { icon: <Youtube className="h-5 w-5 text-red-500" />, label: "YouTube", href: "https://www.youtube.com/@idleducation" }
+                          { icon: <Instagram className="h-5 w-5 text-gray-400" />, label: "Instagram", href: "https://www.instagram.com/idleducation" },
+                          { icon: <Facebook className="h-5 w-5 text-gray-400" />, label: "Facebook", href: "https://www.facebook.com/idleducation" },
+                          { icon: <Twitter className="h-5 w-5 text-gray-400" />, label: "Twitter", href: "https://x.com/idleducation" },
+                          { icon: <Youtube className="h-5 w-5 text-gray-400" />, label: "YouTube", href: "https://www.youtube.com/@idleducation" }
                         ].map((social) => (
                           <Link 
                             key={social.label}
@@ -107,15 +107,15 @@ export function Footer() {
                     <h3 className="text-xs font-black text-white uppercase tracking-[0.3em] mb-8 border-l-4 border-primary pl-3">Contact</h3>
                     <div className="flex flex-col items-start gap-1 text-[11px] font-medium tracking-widest text-gray-400">
                         <a href="tel:+917011117585" className="flex items-center gap-3 hover:text-white transition-colors group w-full justify-start py-1">
-                          <Phone className="h-4 w-4 text-sky-400 shrink-0" />
+                          <Phone className="h-4 w-4 text-gray-400 shrink-0" />
                           <span>+91 7011117585</span>
                         </a>
                         <a href="https://wa.me/918860040010" target="_blank" rel="noopener noreferrer" className="flex items-center gap-3 hover:text-white transition-colors group w-full justify-start py-1">
-                          <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-green-500 shrink-0" viewBox="0 0 24 24" fill="currentColor"><path d="M12.04 2C6.58 2 2.14 6.44 2.14 11.9c0 1.75.46 3.45 1.32 4.95L2 22l5.25-1.38c1.45.79 3.08.12 4.79.12h.01c5.46 0 9.9-4.44 9.9-9.9S17.5 2 12.04 2zM12.04 20.1c-1.55 0-3.04-.49-4.28-1.38l-.3-.18-3.18.84.85-3.1-.19-.31c-.98-1.56-1.5-3.39-1.5-5.28 0-4.51 3.67-8.18 8.18-8.18s8.18 3.67 8.18 8.18-3.67 8.18-8.18 8.18zm4.49-5.37c-.27-.13-1.59-.78-1.84-.87-.25-.09-.43-.13-.62.13-.19.27-.7.87-.86 1.04-.16.18-.32.19-.59.06-.27-.13-1.15-.42-2.18-1.34s-1.66-2.09-1.86-2.43c-.2-.35-.02-.54.12-.68.12-.13.27-.32.4-.43.14-.11.18-.18.27-.3.09-.12.05-.23-.02-.32-.07-.09-.62-1.49-.85-2.04-.23-.55-.46-.48-.62-.48-.15 0-.32-.02-.49-.02s-.43.06-.65.3c-.22.25-.85.83-.85 2.02s.87 2.35 1 2.51c.13.16.85 1.35 2.98 2.62.5.31.89.49 1.2.62.5.21.94.18 1.3.11.39-.07 1.15-.47 1.32-.92.16-.45.16-.83.11-.92-.05-.09-.18-.13-.45-.26z"/></svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-400 shrink-0" viewBox="0 0 24 24" fill="currentColor"><path d="M12.04 2C6.58 2 2.14 6.44 2.14 11.9c0 1.75.46 3.45 1.32 4.95L2 22l5.25-1.38c1.45.79 3.08.12 4.79.12h.01c5.46 0 9.9-4.44 9.9-9.9S17.5 2 12.04 2zM12.04 20.1c-1.55 0-3.04-.49-4.28-1.38l-.3-.18-3.18.84.85-3.1-.19-.31c-.98-1.56-1.5-3.39-1.5-5.28 0-4.51 3.67-8.18 8.18-8.18s8.18 3.67 8.18 8.18-3.67 8.18-8.18 8.18zm4.49-5.37c-.27-.13-1.59-.78-1.84-.87-.25-.09-.43-.13-.62.13-.19.27-.7.87-.86 1.04-.16.18-.32.19-.59.06-.27-.13-1.15-.42-2.18-1.34s-1.66-2.09-1.86-2.43c-.2-.35-.02-.54.12-.68.12-.13.27-.32.4-.43.14-.11.18-.18.27-.3.09-.12.05-.23-.02-.32-.07-.09-.62-1.49-.85-2.04-.23-.55-.46-.48-.62-.48-.15 0-.32-.02-.49-.02s-.43.06-.65.3c-.22.25-.85.83-.85 2.02s.87 2.35 1 2.51c.13.16.85 1.35 2.98 2.62.5.31.89.49 1.2.62.5.21.94.18 1.3.11.39-.07 1.15-.47 1.32-.92.16-.45.16-.83.11-.92-.05-.09-.18-.13-.45-.26z"/></svg>
                           <span>+91 8860040010</span>
                         </a>
                         <a href="mailto:info@idleducation.in" className="flex items-center gap-3 hover:text-white transition-colors group w-full justify-start py-1">
-                          <Mail className="h-4 w-4 text-rose-400 shrink-0" />
+                          <Mail className="h-4 w-4 text-gray-400 shrink-0" />
                           <span>info@idleducation.in</span>
                         </a>
                     </div>
