02a4a8f56eac676c4e9a7370828a164953d54df4
Amod Sharma <kumarvips.in@gmail.com>
2026-02-08 05:07:11 +0000

    Try fixing this error: `Runtime ReferenceError: Skeleton is not defined.

--- src/app/examcat/page.tsx
+++ src/app/examcat/page.tsx
@@ -20,6 +20,7 @@ import Image from "next/image";
 import { useSearchParams, useRouter } from 'next/navigation';
 import { cn } from "@/lib/utils";
 import { Badge } from "@/components/ui/badge";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const CheckIcon = () => (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
