57cfff8868b1ac7a4246400304313a6336f97f93
Amod Sharma <kumarvips.in@gmail.com>
2026-02-26 03:03:38 +0000

    Try fixing this error: `Runtime ReferenceError: Label is not defined. Er

--- src/app/free-courses/free-courses-client.tsx
+++ src/app/free-courses/free-courses-client.tsx
@@ -14,6 +14,7 @@ import { cn } from "@/lib/utils";
 import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const VideoItem = ({
     video,
